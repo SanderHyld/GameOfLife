a44a852162449a9557d57745c2a9585f5adbc0dd
SanderHyld <sander_hyldbakk@hotmail.no>
2022-04-28 00:39:15 +0200

    Improved function descriptor
    Small update, removed old notes and changed description

--- TestCell.py
+++ TestCell.py
@@ -1,11 +1,6 @@
 def TestNeighbours(xx, yy, prevBoard) -> bool:
-    # Checks neighbours at a specic coordinate and returns a bool based on the GoL rules
-    """
-    Board details
-    (xx-1, yy-1)  (xx, yy-1)  (xx+1, yy-1)
-    (xx-1, yy)    (xx, yy)    (xx+1, yy)
-    (xx-1, yy+1)  (xx, yy+1)  (xx+1, yy+1)
-    """
+    """Checks neighbours at a specic coordinate and returns a bool based on the GoL rules"""
+    
     neighbour = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
