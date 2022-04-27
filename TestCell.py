2f6110302a63ecb3ed99351f02bb7901ec98f539
SanderHyld <sander_hyldbakk@hotmail.no>
2022-04-28 00:44:16 +0200

    Variable disambiguation
    Minor change to variable name. Nitpicking.

--- TestCell.py
+++ TestCell.py
@@ -1,24 +1,24 @@
-def TestNeighbours(xx, yy, prevBoard) -> bool:
+def TestNeighbours(x, y, prevBoard) -> bool:
     """Checks neighbours at a specic coordinate and returns a bool based on the GoL rules"""
-    
+
     neighbour = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
             if (i != 0 or j != 0):
                 try:
-                    if (yy+j != -1):
-                        if (xx+i != -1):
-                            if prevBoard[yy + j][xx + i]:
+                    if (y+j != -1):
+                        if (x+i != -1):
+                            if prevBoard[y + j][x + i]:
                                 neighbour += 1
                 except:
                     pass
 
-    if prevBoard[yy][xx]:
+    if prevBoard[y][x]:
         # rule 1 states that living cell with 2-3 neighbours survive
         if neighbour in (2, 3):
             return True
 
-    if not prevBoard[yy][xx]:
+    if not prevBoard[y][x]:
         # rule 2 states that dead cell with 3 neighbours come to life
         if neighbour == 3:
             return True
